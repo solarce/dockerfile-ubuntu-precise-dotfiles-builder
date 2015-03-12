4a81dc8c9a86d9fb8df03af454c848a6fcc5a0d5
Brandon Burton <brandon@inatree.org>
2015-03-12 13:43:07 -0400

    removing redundant step

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,6 @@ export SH=/bin/bash
 git clone --depth 1 https://github.com/jsonn/pkgsrc.git
 cd ~/pkgsrc/bootstrap
 ./bootstrap --unprivileged
-cd ~/pkgsrc/pkgtools/pkg_install && ~/pkg/bin/bmake install clean clean-depends
 cd ~/pkgsrc/pkgtools/pkg_rolling-replace && ~/pkg/bin/bmake install clean clean-depends
 cd ~/pkgsrc/pkgtools/pkg_chk && ~/pkg/bin/bmake install clean clean-depends
 EOSU
