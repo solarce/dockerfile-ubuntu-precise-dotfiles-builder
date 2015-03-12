24b38dba8a5753cb9698f13ab3a36fafeffa6b69
Brandon Burton <brandon@inatree.org>
2015-03-12 13:41:19 -0400

    cleaning up some debugging code from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,9 @@
 su ubuntu <<EOSU
 cd /home/ubuntu
 export SH=/bin/bash
-whoami
-pwd
 git clone --depth 1 https://github.com/jsonn/pkgsrc.git
 cd ~/pkgsrc/bootstrap
-pwd
 ./bootstrap --unprivileged
-ls -lah ~/pkg/
 cd ~/pkgsrc/pkgtools/pkg_install && ~/pkg/bin/bmake install clean clean-depends
 cd ~/pkgsrc/pkgtools/pkg_rolling-replace && ~/pkg/bin/bmake install clean clean-depends
 cd ~/pkgsrc/pkgtools/pkg_chk && ~/pkg/bin/bmake install clean clean-depends
